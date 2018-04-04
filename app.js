a6909765516bb57b41a091f982b0bc2e66d49dbb
ingridsag <starlling@hotmail.com>
2018-04-04 17:22:23 -0500

    subiendo repo con primeros archivos

---
+++ app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $("ul.tabs").tabs();
+});
+
+/*window.onload = function() { 
+	$("#chartContainer").CanvasJSChart({ 
+		title: { 
+			text: "Presupuesto para campañas electorales 2018 asignado a Partidos y candidatos independientes",
+			fontSize: 24
+		}, 
+		axisY: { 
+			title: "Products in %" 
+		}, 
+		legend :{ 
+			verticalAlign: "center", 
+			horizontalAlign: "right" 
+		}, 
+		data: [ 
+		{ 
+			type: "pie", 
+			showInLegend: true, 
+			toolTipContent: "{label} <br/> {y} %", 
+			indexLabel: "{y} %", 
+			dataPoints: [ 
+				{ label: "Samsung",  y: 30.3, legendText: "Samsung"}, 
+				{ label: "Apple",    y: 19.1, legendText: "Apple"  }, 
+				{ label: "Huawei",   y: 4.0,  legendText: "Huawei" }, 
+				{ label: "LG",       y: 3.8,  legendText: "LG Electronics"}, 
+				{ label: "Lenovo",   y: 3.2,  legendText: "Lenovo" }, 
+				{ label: "Others",   y: 39.6, legendText: "Others" } 
+			] 
+		} 
+		] 
+	}); 
+} */
